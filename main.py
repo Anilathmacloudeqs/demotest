ae70453c39278e72570a8c8581be769cdb5f77c7
Anilathmacloudeqs <129284639+Anilathmacloudeqs@users.noreply.github.com>
2023-12-22 18:14:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,16 @@
 import requests
-import base64
 
-
-def push_file_to_branch(username, repository, source_branch, destination_branch, access_token, file_path,
-                        commit_message):
-    # API URLs for the source and destination branches
-    source_api_url = f'https://api.github.com/repos/{username}/{repository}/contents/{file_path}?ref={source_branch}'
-    destination_api_url = f'https://api.github.com/repos/{username}/{repository}/contents/{file_path}?ref={destination_branch}'
-
+def list_files_in_repo(username, repository, branch, access_token):
+    api_url = f'https://api.github.com/repos/{username}/{repository}/contents/'
     headers = {'Authorization': f'token {access_token}'}
+    response = requests.get(api_url + '?ref=' + branch, headers=headers)
 
-    source_file_response = requests.get(source_api_url, headers=headers)
-    print(source_api_url)
-
-    if source_file_response.status_code == 200:
-        # Get the current commit SHA of the source file
-        source_file_content = source_file_response.json()
-        source_commit_sha = source_file_content['sha']
-
-        # Encode file content in base64
-        encoded_content = base64.b64decode(source_file_content['content']).decode()
-
-        # Prepare payload for creating a new file in the destination branch
-        payload = {
-            'message': commit_message,
-            'content': base64.b64encode(encoded_content.encode()).decode(),
-            'branch': destination_branch,
-            'sha': source_commit_sha
-        }
-
-        # Create or update the file in the destination branch
-        response = requests.put(destination_api_url, headers=headers, json=payload)
-
-        if response.status_code == 201 or response.status_code == 200:
-            print(f"File '{file_path}' successfully pushed to the '{destination_branch}' branch.")
-        else:
-            print(f"Error: Unable to push file. Status code: {response.status_code}")
-            print(response.json())
+    if response.status_code == 200:
+        files = response.json()
+        for file in files:
+            print(file['name'])
     else:
-        print(f"Error: Unable to fetch source file. Status code: {source_file_response.status_code}")
+        print(f"Error: Unable to fetch files. Status code: {response.status_code}")
 
-
-
-push_file_to_branch('Anilathmacloudeqs', 'demotest', 'main', 'release', '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',
-                    'hello.py', 'Commit message')
+# Replace 'Anilathmacloudeqs', 'demotest', 'main', and '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' with your actual GitHub username, repository name, branch name, and access token
+list_files_in_repo('Anilathmacloudeqs', 'demotest', 'release', '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')
